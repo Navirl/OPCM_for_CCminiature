9f944325836ef3e1b7a181be4d35be7626ebe8b2
Seika <37077875+Navirl@users.noreply.github.com>
2026-02-27 23:41:52 +0900

    feat:add for test

---
+++ robot.lua
@@ -0,0 +1,12 @@
+
+
+return {
+    select = function(v) print("select!")end,
+    placeDown = function(v) print("placedown!") end,
+    left = function(v) print("left!") end,
+    up = function(v) print("up!") end,
+    north = function(v) print("north!") end,
+    south = function(v) print("south!") end,
+    dropDown = function(v) print("dropDown!") end,
+    down = function(v) print("down!") end
+}
---
+++ test.lua
@@ -0,0 +1,4 @@
+local m = require("make")
+local e = require("ender_pearl")
+
+m.make(e,3)
