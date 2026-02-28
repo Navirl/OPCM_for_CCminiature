3f328b7747ba3e7d419693c1a4f111d5d07d8150
Seika <37077875+Navirl@users.noreply.github.com>
2026-02-28 23:15:01 +0900

    fix: robot movement

--- make.lua
+++ make.lua
@@ -1,8 +1,8 @@
 local structure = require("data.structure")
 local robot = require("robot")
 
-local function getSlotNumFromTable(toolbar_items,item_name)
-    for i,v in ipairs(toolbar_items) do
+local function getSlotNumFromTable(toolbar_items, item_name)
+    for i, v in ipairs(toolbar_items) do
         local name, num = string.match(v, "([^:]+):(%d+)")
         if name == item_name then
             local slotnum = i
@@ -14,7 +14,7 @@ end
 local make = function(structure_table, proj_light_size)
     local meta_structure_table = getmetatable(structure_table).__index
     if meta_structure_table ~= structure then
-        error("set structure is unknown data."..structure..meta_structure_table)
+        error("set structure is unknown data." .. structure .. meta_structure_table)
     end
 
     local toolbar_items = structure_table:toolbarItems()
@@ -25,33 +25,56 @@ local make = function(structure_table, proj_light_size)
         error("set structure is too giant about projector size.")
     end
 
-    robot.right(2)
+    --[[
+    rをロボット、xを光とし、最下段のこの位置に置く
+    一例として3x3x3の光
+
+        r--xxx
+           xxx
+           xxx
+    
+    ]]
+    robot.forward()
+    robot.forward()
     for i1, v1 in pairs(structure_table.proj_struct) do
         if v1 ~= "" then
-            robot.select(getSlotNumFromTable(toolbar_items,v1))
+            robot.select(getSlotNumFromTable(toolbar_items, v1))
             robot.placeDown(v1)
         end
 
         if i1 % edge_size == 0 then
-            robot.left(edge_size - 1)
+            for _i = 1, edge_size - 1, 1 do
+                robot.back()
+            end
             if i1 % face_size == 0 then
-                robot.north(edge_size - 1)
-                robot.up(1)
+                robot.turnLeft()
+                for _i = 1, edge_size - 1, 1 do
+                    robot.forward()
+                end
+                robot.up()
+                robot.turnRight()
             else
-                robot.south(1)
+                robot.turnRight()
+                robot.forward()
+                robot.turnLeft()
             end
         else
-            robot.right(1)
+            robot.forward()
         end
     end
 
     if size_diff >= 0 then
-        robot.up(size_diff)
+        for _i = 1, size_diff, 1 do
+            robot.up()
+        end
     end
 
     robot.dropDown(structure_table.drop_item)
-    robot.left(2)
-    robot.down(proj_light_size)
+    robot.back()
+    robot.back()
+    for _i = 1, proj_light_size, 1 do
+        robot.down()
+    end
 end
 
 return {
