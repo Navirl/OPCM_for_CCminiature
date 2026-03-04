72cc6c72c9f085b000f4001d2868ba0a0ffb8a12
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-04 14:32:58 +0900

    add: machine wall data

---
+++ data/machine_wall.lua
@@ -0,0 +1,15 @@
+local structure = require("data.structure")
+
+local machine_wall = structure.new({
+proj_struct = {
+                    "iron_block","",
+                    "","",
+
+                    "redstone/use","",
+                    "","",
+},
+drop_item = "redstone",
+detect_item = "iron_block"
+})
+
+return machine_wall
--- datalist.lua
+++ datalist.lua
@@ -1,7 +1,9 @@
 local ender_pearl = require("ender_pearl")
+local machine_wall = require("machine_wall")
 
 local toolbar_list = {}
 table.insert(toolbar_list, ender_pearl.toolbar_items)
+table.insert(toolbar_list, machine_wall.toolbar_items)
 
 return {
     toolbar_list = toolbar_list
