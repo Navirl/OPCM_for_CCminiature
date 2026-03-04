a389c155770868d2ab7520ef6993fab0c3f3c4d8
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-04 13:51:28 +0900

    refactor: in front of basic command

--- make.lua
+++ make.lua
@@ -2,6 +2,9 @@ local structure = require("data.structure")
 local robot = require("robot")
 
 local function getSlotNumFromTable(toolbar_items, item_name)
+    if item_name == "" then
+        return nil
+    end
     for i, v in ipairs(toolbar_items) do
         local name, num = string.match(v, "([^:]+):(%d+)")
         if name == item_name then
@@ -37,29 +40,32 @@ local make = function(structure_table, proj_light_size)
     robot.forward()
     robot.forward()
     for i1, v1 in pairs(structure_table.proj_struct) do
+        -- 無ければ何もしない
         if v1 ~= "" then
-            robot.select(getSlotNumFromTable(toolbar_items, v1))
+            local slot_num = getSlotNumFromTable(toolbar_items, v1)
+            robot.select(slot_num)
             robot.placeDown()
         end
-
-        if i1 % edge_size == 0 then
+        -- 端までは設置後前進
+        if i1 % edge_size ~= 0 then
+            robot.forward()
+        else
             for _i = 1, edge_size - 1, 1 do
                 robot.back()
             end
-            if i1 % face_size == 0 then
+            -- 面までは次の行へ移動
+            if i1 % face_size ~= 0 then
+                robot.turnRight()
+                robot.forward()
+                robot.turnLeft()
+            else
                 robot.turnLeft()
                 for _i = 1, edge_size - 1, 1 do
                     robot.forward()
                 end
                 robot.up()
                 robot.turnRight()
-            else
-                robot.turnRight()
-                robot.forward()
-                robot.turnLeft()
             end
-        else
-            robot.forward()
         end
     end
 
@@ -68,7 +74,8 @@ local make = function(structure_table, proj_light_size)
             robot.up()
         end
     end
-    robot.select(getSlotNumFromTable(toolbar_items, structure_table.drop_item))
+    local slot_num = getSlotNumFromTable(toolbar_items, structure_table.drop_item)
+    robot.select(slot_num)
     robot.dropDown(1)
     robot.back()
     robot.back()
