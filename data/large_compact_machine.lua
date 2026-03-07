6dbd9e23fc21b744d972ee1dba626044bf3f7e01
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-08 08:35:37 +0900

    add: large_compact_machine

---
+++ data/large_compact_machine.lua
@@ -0,0 +1,40 @@
+local structure = require("data.structure")
+
+local large_compact_machine = structure.new({
+proj_struct = {
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+                    "machine_wall","machine_wall","machine_wall","machine_wall","machine_wall",
+
+},
+drop_item = "ender_pearl",
+detect_item = "obsidian"
+})
+
+return large_compact_machine
