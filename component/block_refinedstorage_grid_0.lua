00bf7d8cd48b62bcd0a4a7e9d302ba0ff424cb5e
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-12 00:28:24 +0900

    test: add machine_wall

--- component/block_refinedstorage_grid_0.lua
+++ component/block_refinedstorage_grid_0.lua
@@ -1,8 +1,29 @@
+local json = require("cjson")
+
+-- キーを数値に変換する関数
+-- jsonのキーは文字列型しかないので、これで実際のキー型と合わせてテスト可能にする
+local function keys_to_numbers(t)
+    if type(t) ~= "table" then return t end
+
+    local new_t = {}
+    for k, v in pairs(t) do
+        -- キーが文字列で、かつ数値に変換可能な場合
+        local num_key = tonumber(k)
+        if type(k) == "string" and num_key then
+            new_t[num_key] = keys_to_numbers(v)
+        else
+            new_t[k] = keys_to_numbers(v)
+        end
+    end
+    return new_t
+end
+
 return {
     getTasks = function()
-        return {
-            [1] = { ["quantity"] = 2.0,["missingFluids"] = { ["n"] = 0,} ,["missing"] = { ["n"] = 0,} ,["stack"] = { ["item"] = { ["maxDamage"] = 0.0,["size"] = 1.0,["name"] = "minecraft:ender_pearl",["maxSize"] = 64.0,["label"] = "Mushroom",["hasTag"] = false,["damage"] = 0.0,} ,} ,["pattern"] = { ["processing"] = true,["fluidOutputs"] = { ["n"] = 0,} ,["fluidInputs"] = { ["n"] = 0,} ,["oredict"] = false,["inputs"] = { [1] = { [1] = { ["maxDamage"] = 0.0,["size"] = 26.0,["name"] = "minecraft:obsidian",["maxSize"] = 64.0,["label"] = "Lapis Lazuli",["hasTag"] = false,["damage"] = 4.0,} ,["n"] = 1,} ,[2] = { [1] = { ["maxDamage"] = 0.0,["size"] = 1.0,["name"] = "minecraft:redstone_block",["maxSize"] = 64.0,["label"] = "Gold Ingot",["hasTag"] = false,["damage"] = 0.0,} ,["n"] = 1,} ,[3] = { [1] = { ["maxDamage"] = 0.0,["size"] = 1.0,["name"] = "minecraft:redstone",["maxSize"] = 64.0,["label"] = "Gold Ingot",["hasTag"] = false,["damage"] = 0.0,} ,["n"] = 1,}  ,[4] = { ["n"] = 0,} ,[5] = { ["n"] = 0,} ,[6] = { ["n"] = 0,} ,[7] = { ["n"] = 0,} ,[8] = { ["n"] = 0,} ,[9] = { ["n"] = 0,} ,["n"] = 9,} ,["outputs"] = { [1] = { ["maxDamage"] = 0.0,["size"] = 1.0,["name"] = "minecraft:ender_pearl",["maxSize"] = 64.0,["label"] = "Mushroom",["hasTag"] = false,["damage"] = 0.0,} ,["n"] = 1,} ,} ,} ,["n"] = 1,
-        }
+        local file = io.open("tasks.json", "r")
+        local tasks = json.decode(file:read("a"))
+        local num_tasks = keys_to_numbers(tasks)
+        return num_tasks
     end,
     getItem = function(v)
         return {size = 10000.0}
--- tests/test.lua
+++ tests/test.lua
@@ -1,5 +1,6 @@
 ---@diagnostic disable: undefined-global, undefined-field
 local ep = require("data.ender_pearl")
+local mw = require("data.machine_wall")
 local grid = require("component.block_refinedstorage_grid_0")
 local make = require("make")
 local eg = require("extract_grid")
@@ -8,17 +9,29 @@ local p = require("tprint")
 
 --- make.make(e,3)
 describe("Does make work?", function()
+    local tasks = grid.getTasks()
     it("ender_pearl", function()
-        local tasks = grid.getTasks()
         local num = eg.getCapableTaskNum("ender_pearl", tasks)
         local toolbar = eg.calcToolbaraboutExtractableTaskItems(tasks[num])
         local s = stub(_G, "print")
-        make.make(ep,toolbar,3)
+        make(ep,toolbar,3)
         local print_all = ""
         for _, call in ipairs(s.calls) do
             print_all = print_all .. tostring(call.vals[1]) .. "\n"
         end
-        -- 最後の改行だけ消す
-        assert.are.equals(string.sub(print_all,1,-2), "forward!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 2\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nselect! 3\ndropDown! 1\nback!\nback!\ndown!\ndown!\ndown!")
+        assert.are.equals(print_all, "forward!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 2\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnRight!\nforward!\nturnLeft!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nforward!\nselect! 1\nplacedown!\nback!\nback!\nturnLeft!\nforward!\nforward!\nturnRight!\nup!\nback!\nback!\ndown!\ndown!\ndown!\nselect! 3\ndrop! 1\n")
+        s:clear()
+        end)
+
+    it("machine_wall", function()
+        local num = eg.getCapableTaskNum("machine_wall", tasks)
+        local toolbar = eg.calcToolbaraboutExtractableTaskItems(tasks[num])
+        local s = stub(_G, "print")
+        make(mw,toolbar,3)
+        local print_all = ""
+        for _, call in ipairs(s.calls) do
+            print_all = print_all .. tostring(call.vals[1]) .. "\n"
+        end
+        assert.are.equals(print_all, "forward!\nforward!\nselect! 1\nplacedown!\nforward!\nback!\nturnRight!\nforward!\nturnLeft!\nforward!\nback!\nturnLeft!\nforward!\nturnRight!\nup!\nselect! 2\nplacedown!\nforward!\nback!\nturnRight!\nforward!\nturnLeft!\nforward!\nback!\nturnLeft!\nforward!\nturnRight!\nup!\nback!\nback!\ndown!\ndown!\nselect! 2\ndrop! 1\n")
     end)
 end)
