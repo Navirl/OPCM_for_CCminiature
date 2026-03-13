159d4dd26bced44bc6ff8f845e8d30a93d157a1a
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-13 13:54:27 +0900

    fix: relocate test files

---
+++ component.lua
@@ -0,0 +1,5 @@
+return {
+    block_refinedstorage_grid_0 = require('tests.component.block_refinedstorage_grid_0'),
+    transposer = require('tests.component.transposer'),
+    tunnel = require('tests.component.tunnel'),
+}
