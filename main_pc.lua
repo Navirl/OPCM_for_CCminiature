9878870890007a9eeba46e5903a4e01a503affd6
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-13 17:10:46 +0900

    feat: infinite

--- main_pc.lua
+++ main_pc.lua
@@ -8,53 +8,54 @@ local sides = require("sides")
 local tunnel = c.tunnel
 local json = require("json")
 
-
-print("信号を待機中...")
-
--- レッドストーン信号（redstone_changed）が変わるまで一時停止
 while true do
-  local _, _, _, _, newValue = event.pull("redstone_changed")
-  if newValue > 0 then
-    print("信号検知！作業を開始します。")
-    break
-  end
-end
+    print("信号を待機中...")
 
-local tasks = grid.getTasks()
-local task_num = 0
-local item_name = ""
--- 作成可能なタスク探し
-for i, v in ipairs(dl.item_names) do
-    task_num = eg.getMakingIteminTaskNum(v,tasks)
-    if task_num ~= "No making that item in tasks." then
-        item_name = v
-        break
-    end
-end
-if task_num == 0 or task_num == "No making that item in tasks." then
-    return print("No making that item in tasks.")
-end
-
--- 作成可能なタスクがアイテムを何個作ろうとしてるか、そこから必要なアイテムのセット数を取る
-local set_num = tasks[task_num]["quantity"]
-for i=1,set_num do
-    -- gridからchestへ
-    local toolbar_items = eg.calcToolbaraboutExtractableTaskItems(tasks[task_num])
-
-    for i2,v2 in ipairs(toolbar_items) do
-        eg.extractItemFromGrid(v2,sides.north)
-        -- chestからrobotへ
-        ec(sides.south,sides.up,i2)
+    -- レッドストーン信号（redstone_changed）が変わるまで一時停止
+    while true do
+        local _, _, _, _, newValue = event.pull("redstone_changed")
+        if newValue > 0 then
+            print("信号検知！作業を開始します。")
+            break
+        end
     end
 
-    -- ロボットにやってもらう
-    tunnel.send("make^"..item_name.."^"..json.encode(toolbar_items))
-
-    -- ロボットから終了を受け取る
-    local _, _, _, _, _, recv_data = event.pull("modem_message")
-    -- 終了以外を受け取ったらエラー、終了なら次のextractに進む
-    if recv_data ~= "finished" then
-        return "error"..recv_data
+    local tasks = grid.getTasks()
+    local task_num = 0
+    local item_name = ""
+    -- 作成可能なタスク探し
+    for i, v in ipairs(dl.item_names) do
+        task_num = eg.getMakingIteminTaskNum(v, tasks)
+        if task_num ~= "No making that item in tasks." then
+            item_name = v
+            break
+        end
     end
-    print("set " .. i)
+    if task_num == 0 or task_num == "No making that item in tasks." then
+        return print("No making that item in tasks.")
+    end
+
+    -- 作成可能なタスクがアイテムを何個作ろうとしてるか、そこから必要なアイテムのセット数を取る
+    local set_num = tasks[task_num]["quantity"]
+    for i = 1, set_num do
+        -- gridからchestへ
+        local toolbar_items = eg.calcToolbaraboutExtractableTaskItems(tasks[task_num])
+
+        for i2, v2 in ipairs(toolbar_items) do
+            eg.extractItemFromGrid(v2, sides.north)
+            -- chestからrobotへ
+            ec(sides.south, sides.up, i2)
+        end
+
+        -- ロボットにやってもらう
+        tunnel.send("make^" .. item_name .. "^" .. json.encode(toolbar_items))
+
+        -- ロボットから終了を受け取る
+        local _, _, _, _, _, recv_data = event.pull("modem_message")
+        -- 終了以外を受け取ったらエラー、終了なら次のextractに進む
+        if recv_data ~= "finished" then
+            return "error" .. recv_data
+        end
+        print("set " .. i)
+    end
 end
--- main_robot.lua
+++ main_robot.lua
@@ -2,24 +2,29 @@ local event = require("event")
 local make = require("make")
 local json = require("json")
 local dl = require("datalist")
+local c = require("component")
+local tunnel = c.tunnel
 
--- pcから作成依頼を受け取る
-local item_name_e = ""
-local toolbar_item_e = ""
 while true do
-    local _, _, _, _, _, recv_data = event.pull("modem_message")
-    -- 作成依頼があるなら、どれを作るかが必要
-    -- datalistから取り出せば良いんだけど
-    -- ツールバー情報も依頼で受け取れ
-    local inst, item_name, toolbar_item_raw =
-        string.match(recv_data, "([^%^]+)^(.+)^(.+)")
-    ---@diagnostic disable-next-line: assign-type-mismatch
-    item_name_e = item_name
-    toolbar_item_e = json.decode(toolbar_item_raw)
-    if inst == "make" then
-        print("作成開始")
-        break
+    -- pcから作成依頼を受け取る
+    local item_name_e = ""
+    local toolbar_item_e = ""
+    while true do
+        local _, _, _, _, _, recv_data = event.pull("modem_message")
+        -- 作成依頼があるなら、どれを作るかが必要
+        -- datalistから取り出せば良いんだけど
+        -- ツールバー情報も依頼で受け取れ
+        local inst, item_name, toolbar_item_raw =
+            string.match(recv_data, "([^%^]+)^(.+)^(.+)")
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        item_name_e = item_name
+        toolbar_item_e = json.decode(toolbar_item_raw)
+        if inst == "make" then
+            print("作成開始")
+            break
+        end
     end
-end
 
-make(dl.proj_structs[item_name_e], toolbar_item_e, 3)
+    make(dl.proj_structs[item_name_e], toolbar_item_e, 3)
+    tunnel.send("finished")
+end
