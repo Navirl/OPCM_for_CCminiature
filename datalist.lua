11a367e55f09d3eaf389b97b6730c5fd882fc2ff
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-13 11:54:27 +0900

    add: main_robot

--- datalist.lua
+++ datalist.lua
@@ -5,6 +5,11 @@ local item_names = {}
 table.insert(item_names, ender_pearl.item_name)
 table.insert(item_names, machine_wall.item_name)
 
+local structures = {}
+structures[ender_pearl.item_name] = ender_pearl
+structures[machine_wall.item_name] = machine_wall
+
 return {
-    item_names = item_names
+    item_names = item_names,
+    proj_structs = structures,
 }
