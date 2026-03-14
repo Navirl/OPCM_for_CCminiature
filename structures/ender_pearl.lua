c12d406166d88bb28227241bb091649deeebba1d
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-15 00:44:53 +0900

    fix: change sleep time

--- structures/ender_pearl.lua
+++ structures/ender_pearl.lua
@@ -2,7 +2,7 @@ local structure = require('structures.structure')
 
 local ender_pearl = structure.new({
     item_name = "Ender Pearl",
-    sleep_time = 8,
+    sleep_time = 10,
     proj_struct = {
         "minecraft:obsidian", "minecraft:obsidian", "minecraft:obsidian",
         "minecraft:obsidian", "minecraft:obsidian", "minecraft:obsidian",
