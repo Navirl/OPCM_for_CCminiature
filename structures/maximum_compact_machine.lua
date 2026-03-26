415fb8b24f5ba5b59947b95adfe06e055f5701e5
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-26 13:18:44 +0900

    fix: change maximum 3 to 5

--- structures/maximum_compact_machine.lua
+++ structures/maximum_compact_machine.lua
@@ -4,19 +4,36 @@ local maximum_compact_machine = structure.new({
     item_name = "Maximum Compact Machine",
     sleep_time = 90,
     proj_struct = {
-        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
-        "compactmachines3:wallbreakable", "contenttweaker:glitched4", "compactmachines3:wallbreakable",
-        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
 
-        "compactmachines3:wallbreakable", "contenttweaker:glitched4", "compactmachines3:wallbreakable",
-        "contenttweaker:glitched4", "modularmachinery:blockcasing", "contenttweaker:glitched4",
-        "compactmachines3:wallbreakable", "contenttweaker:glitched4", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "contenttweaker:glitched4", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
 
-        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
-        "compactmachines3:wallbreakable", "contenttweaker:glitched4", "compactmachines3:wallbreakable",
-        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
 
-    },
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+        "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
+    }
 })
 
 return maximum_compact_machine
