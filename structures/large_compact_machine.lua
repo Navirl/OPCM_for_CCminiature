ee1b8b6f7d862ae23fbe9a1b6381d41a87787397
Seika <37077875+Navirl@users.noreply.github.com>
2026-03-27 11:36:18 +0900

    fix: finetune wait time in large

--- structures/large_compact_machine.lua
+++ structures/large_compact_machine.lua
@@ -2,7 +2,7 @@ local structure = require('structures.structure')
 
 local large_compact_machine = structure.new({
     item_name = "Large Compact Machine",
-    sleep_time = 12,
+    sleep_time = 30,
     proj_struct = {
         "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
         "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable", "compactmachines3:wallbreakable",
